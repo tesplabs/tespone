9fcf280d435f8329c15d3d3479533dca622ccc91
Ashwin Kannoth <ashwin.kannoth@tesplabs.com>
2026-03-24 10:39:46 +0530

    apis are integrated all

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ window.appConfig = {
             label: 'Network',
             type: 'form',
             title: 'Ethernet Configuration',
-             api: '/api/ethernetconfiguration', // API endpoint for this form
+            api: '/api/ethernetconfiguration', // API endpoint for this form
             fields: [
                  { name: 'mode', label: 'Mode', type: 'select', options: ['DHCP', 'Static'] },
                  { name: 'ipAddress', label: 'IP Address', type: 'text', placeholder: '192.168.1.100' },
@@ -35,14 +35,14 @@ window.appConfig = {
             label: 'MQTT',
             type: 'form',
             title: 'MQTT Configuration',
-               api: '/api/mqttconfiguration', // API endpoint for this form
-                fields: [
-                    { name: 'brokerUrl', label: 'Broker URL', type: 'text', placeholder: 'mqtt://broker.hivemq.com' },
-                    { name: 'port', label: 'Port', type: 'number', placeholder: '1883' },
-                    { name: 'username', label: 'Username', type: 'text', placeholder: '' },
-                    { name: 'password', label: 'Password', type: 'password', placeholder: '' },
-                    { name: 'secured', label: 'Secured', type: 'checkbox', placeholder: '' },
-                ]
+            api: '/api/mqttconfiguration', // API endpoint for this form
+            fields: [
+                { name: 'brokerUrl', label: 'Broker URL', type: 'text', placeholder: 'mqtt://broker.hivemq.com' },
+                { name: 'port', label: 'Port', type: 'number', placeholder: '1883' },
+                { name: 'username', label: 'Username', type: 'text', placeholder: '' },
+                { name: 'password', label: 'Password', type: 'password', placeholder: '' },
+                { name: 'secured', label: 'Secured', type: 'checkbox', placeholder: '' },
+            ]
         },
         {
             id: 'maintenance',
